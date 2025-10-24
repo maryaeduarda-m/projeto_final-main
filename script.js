a5894aaffb6d263be6b4d529190035a841ab84e0
Maria Eduarda <maryaeduarda2112@gmail.com>
2025-10-24 18:33:02 -0300

    att

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("#formContato");
+const nome = document.getElementById("#nome");
+const telefone = document.getElementById("telefone");
+const email = document.getElementById("email");
+const duvida = document.getElementById("duvida");
+
+
+form.addEventListener("submit", function (evento) {
+  evento.preventDefault});
